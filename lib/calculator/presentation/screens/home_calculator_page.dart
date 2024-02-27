7b9dc05496690f8b6e4519d401002608394d0e65
NourSabry <nourhan.sabrrii@gmail.com>
2024-02-28 00:40:21 +0200

    progress on creating the layout
    Co-authored-by: mennaawalid <mennaawalid@users.noreply.github.com>

--- lib/calculator/presentation/screens/home_calculator_page.dart
+++ lib/calculator/presentation/screens/home_calculator_page.dart
@@ -18,6 +18,21 @@ class _HomeCalculatorPageState extends ConsumerState<HomeCalculatorPage> {
   Widget build(BuildContext context) {
     final themeMode = ref.watch(themeProvider);
     final theme = Theme.of(context);
+    Widget createButton(
+      String text, {
+      int flex = 1,
+      double? height,
+      double? width,
+    }) {
+      return CalculatorButton(
+        text: text,
+        onPressed: () {},
+        height: height ?? 60.px,
+        width: width ?? 62.px,
+        style: theme.textTheme.titleSmall,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
@@ -36,7 +51,7 @@ class _HomeCalculatorPageState extends ConsumerState<HomeCalculatorPage> {
                   ? const Icon(Icons.light_mode)
                   : SvgPicture.asset(
                       'assets/icons/moon.svg',
-                      height: 18.sp,  
+                      height: 18.sp,
                       width: 18.sp,
                     ),
             ),
@@ -45,45 +60,97 @@ class _HomeCalculatorPageState extends ConsumerState<HomeCalculatorPage> {
       ),
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.all(8.0),
+          padding: EdgeInsets.symmetric(horizontal: 7.w),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
             children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+              SizedBox(
+                height: 30.h,
+                child: const Center(
+                  child: Text("hereee"),
+                ),
+              ),
+              Column(
                 children: [
-                  CalculatorButton(
-                    text: "e",
-                    onPressed: () {},
-                    height: 40.px,
-                    style: theme.textTheme.titleSmall,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      createButton("e", height: 25.px),
+                      createButton("π", height: 25.px),
+                      createButton("sin", height: 25.px),
+                      createButton("deg", height: 25.px),
+                    ],
                   ),
-                  CalculatorButton(
-                    text: "π",
-                    onPressed: () {},
-                    height: 40.px,
-                    style: theme.textTheme.titleSmall,
-                  ),
-                  CalculatorButton(
-                    text: "sin",
-                    onPressed: () {},
-                    height: 40.px,
-                    style: theme.textTheme.titleSmall,
-                  ),
-                  CalculatorButton(
-                    text: "deg",
-                    onPressed: () {},
-                    height: 40.px,
-                    style: theme.textTheme.titleSmall,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Flexible(
+                        flex: 3,
+                        child: Column(
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                createButton("AC"),
+                                createButton("esc"),
+                                createButton("/"),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                createButton("7"),
+                                createButton("8"),
+                                createButton("9"),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                createButton("4"),
+                                createButton("5"),
+                                createButton("6"),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                createButton("1"),
+                                createButton("2"),
+                                createButton("3"),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                createButton("0", width: 131.px),
+                                createButton("."),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            createButton("-"),
+                            createButton("*"),
+                            createButton("+", height: 93.5.px),
+                            createButton("=", height: 93.5.px),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
                 ],
               ),
             ],
           ),
         ),
-      )
-
-          ,
+      ),
     );
   }
 }
